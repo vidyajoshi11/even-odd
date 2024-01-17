cb44836fc5519c91a803cc2ae019e1c8e376ffad
vidya Joshi <148187787+vidyajoshi11@users.noreply.github.com>
2024-01-17 22:42:36 +0530

    Create IFelse.java

---
+++ even/IFelse.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class add
+{
+public static void main(String[] args)
+{
+   int a=10;
+   int b=20;
+  System.out.println("sum"+(a+b));
+
+Scanner sc= new Scanner(System.in);
+System.out.println("enter the number");
+int num=sc.nextInt();
+
+if(num%2==0)
+{
+   System.out.println("even no");
+}
+else
+{
+  System.out.println("odd no");
+}
+}
+}   
